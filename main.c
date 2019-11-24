5879cc34b98fb368cab5927184c2681ab2256cfa
Renato Castilho <renato.matos@soulasalle.com.br>
2019-11-24 23:10:29 +0000

    Arquivamento de alunos

--- main.c
+++ main.c
@@ -35,11 +35,17 @@
     };
         
 
-    // Protótipos
+    // Protótipos 
+    // função para mostrar menu na tela e ler opção digitada
+    int lerOpcao();
 
-    //Protótipos de funções de alunos
-    void arquivaAlunos(struct aluno *alunos);
+    // função para iniciar variáveis do programa
+    void iniciaPrograma(int *nAlunos);
+
+    //Protótipos de funções relacionadas a alunos
     struct aluno leAluno();
+    int adicionaAluno(struct aluno aluno, struct aluno *alunos, int *nAlunos);
+    void arquivaAlunos(struct aluno *alunos, int nAlunos);
     
     struct professor CadastraProfessores();
     void arquivaProfessor(struct professor professor);
@@ -50,30 +56,40 @@
     void fecharTurma(int n_alunos);
     void confirmarTurmas();
     void fecharPrograma();
-
-
-    // função para mostrar menu na tela e ler opção digitada
-    int lerOpcao();
     
     // Programa Principal 
     int main (){
-      struct aluno alunos[CAPACIDADE_MAXIMA];
-      struct aula aulas[CAPACIDADE_MAXIMA];
-      struct inscricao inscricoes[CAPACIDADE_MAXIMA];
-      struct professor professores[CAPACIDADE_MAXIMA];
 
+      struct aluno alunos[CAPACIDADE_MAXIMA];
+      int nAlunos;
+      struct aula aulas[CAPACIDADE_MAXIMA];
+      //int nAulas;
+      struct inscricao inscricoes[CAPACIDADE_MAXIMA];
+      //int nInscricoes;
+      struct professor professores[CAPACIDADE_MAXIMA];
+      //int nProfessores;
 
       int opcao = 1;
+
+      iniciaPrograma(&nAlunos);
       
       do{
         opcao = lerOpcao();
         switch (opcao){
-          case 1: // 1- Cadastra aluno
-            if(0); //P.O.G
-            struct aluno novoAluno;
-            //Ler aluno da entrada padrão
+          case 1: // 1- Cadastrar aluno
+            if(0){}; //P.O.G
+
+            //Ler novo aluno da entrada padrão
+            struct aluno novoAluno = leAluno();
 
             //Adicionar a alunos
+            if(adicionaAluno(novoAluno, alunos, &nAlunos)){
+              puts("Cadastro bem sucedido!\n");
+            }
+            else{
+              puts("ERRO! O aluno não foi cadastrado corretamente.");
+            }
+
             break;
 
           case 2: // 2-  Cadastrar professor
@@ -95,9 +111,11 @@
             break;
 
           case 8:  // 8- Fechar o programa
+            arquivaAlunos(alunos, nAlunos);
+            printf("Encerrando o programa\n");
             exit(0);
-            break;
 
+            break;
           default:
             printf("Opcao invalida, tente novamente.\n");
             break;
@@ -110,27 +128,72 @@
 
   int lerOpcao(){
     int op;
-    puts("Academia superação\nDigite a opcao: ");
+    puts("Academia superação");
+    puts("1- Cadastrar aluno");
+    puts("2- Cadastrar professor");
+    puts("3- Cadastrar turma");
+    puts("4- Inscrever aluno");
+    puts("5- Cancelar inscrição");
+    puts("6- Fechar turmas");
+    puts("7- Confirmar turmas");
+    puts("8- Salvar e sair");
+    puts("Digite a opcao: ");
     scanf("%d", &op);
     return op;
   }
 
+  void iniciaPrograma(int *nAlunos){
+    nAlunos = 0;
+    FILE *fAlunos = fopen("alunos.csv", "r");
+    if(fAlunos == NULL){
+      printf("Erro de I/O");
+      exit(1);
+    }
+    //Carregar alunos do arquivo
+    fclose(fAlunos);
+  }
+
+  // Lê da entrada padrão e retorna um novo aluno
   struct aluno leAluno(){
     struct aluno alu;
 
     printf("Cadastro Aluno:");
     printf("\nId: ");
     scanf("%d", &alu.id);
-    printf("\nNome: ");
+    printf("Nome: ");
     scanf("%s", alu.nome);
-    printf("\nCPF: ");
+    printf("CPF: ");
     scanf("%s", alu.cpf);
-    printf("\nTelefone: ");
+    printf("Telefone: ");
     scanf("%s", alu.telefone);
-    printf("\nE-mail: ");
+    printf("E-mail: ");
     scanf("%s", alu.email);
 
     return alu;
+  }
+
+  // Tenta adicionar um novo aluno à lista atual de alunos em memória.
+  // Retorno: 1 se for bem sucedido, caso contrário, 0.
+  int adicionaAluno(struct aluno novoAluno, struct aluno *alunos, int *nAlunos){
+    if(*nAlunos < CAPACIDADE_MAXIMA){
+      alunos[*nAlunos] = novoAluno;
+      *nAlunos = *nAlunos + 1;
+      return 1;
+    }
+    puts("Limite de alunos atingindo!");
+    return 0;
+  }
+
+  void arquivaAlunos(struct aluno *alunos, int nAlunos){
+    FILE *fAlunos = fopen("alunos.csv", "w");
+    if(fAlunos == NULL){
+      printf("Erro de I/O");
+      exit(1);
+    }
+    for(int i = 0; i < nAlunos; i++){
+      fprintf(fAlunos, "%d,%s,%s,%s,%s\n", alunos[i].id, alunos[i].nome, alunos[i].cpf, alunos[i].telefone, alunos[i].email);
+    }
+    fclose(fAlunos);
   }
 
   struct professor cadastraProfessores (){
